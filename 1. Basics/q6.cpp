f43aeb2de768be5cd71f306da96eda464bb9433d
shreyamali01 <shreya.mali@iitgn.ac.in>
2024-12-19 23:59:19 +0530

    printing table using while loop

---
+++ 1. Basics/q6.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int i = 10;
+    while (i>=1){
+        cout << (i*n) << " ";
+        i = i-1;
+    }
+    return 0;
+}

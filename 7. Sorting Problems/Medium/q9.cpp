d6e069670e266ed2adee40e9445abd8b19639dc9
shreyamali01 <shreya.mali@iitgn.ac.in>
2025-05-16 19:06:35 +0530

    superior element

--- 7. Sorting Problems/Medium/q9.cpp
+++ 7. Sorting Problems/Medium/q9.cpp
@@ -1,20 +1,39 @@
 #include <bits/stdc++.h>
+#include <iostream>
+#include <unordered_set>
 using namespace std;
 
-int longestConsecutive(vector<int>& nums) {
-        
+int longestConsecutive(vector<int> &nums)
+{
+    int n = nums.size();
+    unordered_set<int> st;
+    for (int i = 0; i < n; i++)
+    {
+        st.insert(nums[i]);
+    }
+    int long_len = 0;
+    // iterating through the unordered set
+    for (auto x : st)
+    {
+        int len = 1;
+        if (st.find(x - 1) == st.end())
+        {
+            while (st.find(x + 1) != st.end())
+            {
+                len = len + 1;
+                x = x + 1;
+            }
+        }
+
+        long_len = max(len, long_len);
+    }
+    return long_len;
 }
 
-int main() {
-    int n;
-    cin >> n;
-    int val;
-    vector <int> arr;
-    for (int i=0; i<n; i++){
-        cin >> val;
-        arr.push_back(val);
-    }
-    int ans = longestConsecutive(arr);
-    cout << "The ans is " << ans;
+int main()
+{
+    vector<int> nums = {100, 4, 200, 1, 3, 2};
+    int ans = longestConsecutive(nums);
+    cout << "ans " << ans;
     return 0;
 }

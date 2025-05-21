8ac36f67799e6d0042e9dbb23d29fc6549338235
shreyamali01 <shreya.mali@iitgn.ac.in>
2025-05-22 00:44:04 +0530

    4sum optimal solution

---
+++ 7. Sorting Problems/Difficult/q4_optimal.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+vector<vector<int>> fourSum(vector<int> &nums, int target)
+{
+    vector<vector<int>> ans;
+    sort(nums.begin(), nums.end());
+    for (int i = 0; i < nums.size(); i++)
+    {
+        if (i > 0 && nums[i] == nums[i - 1])
+            continue;
+        for (int j = i + 1; j < nums.size(); j++)
+        {
+            if (j > i + 1 && nums[j] == nums[j - 1])
+                continue;
+            int k = j + 1;
+            int l = nums.size() - 1;
+            while (k < l)
+            {
+                long long res = 1LL * nums[i] + nums[j] + nums[k] + nums[l];
+                if (res > target)
+                {
+                    l--;
+                }
+                else if (res < target)
+                {
+                    k++;
+                }
+                else
+                {
+                    vector<int> temp = {nums[i], nums[j], nums[k], nums[l]};
+                    ans.push_back(temp);
+                    k++;
+                    l--;
+                    while (k < l && nums[k] == nums[k - 1])
+                        k++;
+                    while (k < l && nums[l] == nums[l + 1])
+                        l--;
+                }
+            }
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    vector<int> nums = {-1, 0, 1, 2, -1, -4};
+    int target = 0;
+    vector<vector<int>> result = fourSum(nums, target);
+
+    for (int i = 0; i < result.size(); i++)
+    {
+        for (int j = 0; j < result[i].size(); j++)
+        {
+            cout << result[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}

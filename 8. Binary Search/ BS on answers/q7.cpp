6c78976039ec3d5674db88216364a72e3b65365f
shreyamali01 <shreya.mali@iitgn.ac.in>
2025-06-03 15:28:59 +0530

    find kth missing number brute force solution

---
+++ 8. Binary Search/ BS on answers/q7.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int findKthPositive(vector<int> &arr, int k)
+{
+    if (k < arr[0])
+    {
+        return k;
+    }
+    for (int i = 0; i < arr.size(); i++)
+    {
+        if (arr[i] < k)
+        {
+            k++;
+        }
+        else
+        {
+            return k;
+        }
+    }
+    return k;
+}
+
+int main()
+{
+    vector<int> arr = {2, 3, 4, 7, 11};
+    int k = 5;
+    cout << "ans is " << findKthPositive(arr, k);
+}

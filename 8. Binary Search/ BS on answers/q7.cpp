fcb3c21fe0488d42a97d1217d5c1cba89f442511
shreyamali01 <shreya.mali@iitgn.ac.in>
2025-06-03 15:31:39 +0530

    find the kth missing number brute force sol

--- 8. Binary Search/ BS on answers/q7.cpp
+++ 8. Binary Search/ BS on answers/q7.cpp
@@ -10,7 +10,7 @@ int findKthPositive(vector<int> &arr, int k)
     }
     for (int i = 0; i < arr.size(); i++)
     {
-        if (arr[i] < k)
+        if (arr[i] <= k)
         {
             k++;
         }

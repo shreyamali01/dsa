3492a239409bea19e3f18816f8f2639cc24c57c0
shreyamali01 <shreya.mali@iitgn.ac.in>
2025-06-08 14:25:52 +0530

    insert node at head

---
+++ 10. Linked List/1D LinkedList/q6.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+// insert a node at the tail/end of the linked list
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int data1, Node *next1)
+    {
+        data = data1;
+        next = next1;
+    }
+
+    Node(int data1)
+    {
+        data = data1;
+        next = nullptr;
+    }
+};
+
+Node *ConvertArrToLL(vector<int> &arr)
+{
+    Node *head = new Node(arr[0]);
+    Node *mover = head;
+    for (int i = 1; i < arr.size(); i++)
+    {
+        Node *temp = new Node(arr[i]);
+        mover->next = temp;
+        mover = temp;
+    }
+    return head;
+}
+
+void printLL(Node *head)
+{
+    Node *temp = head;
+    while (temp)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
+
+Node *insertElAtEnd(Node *head, int x)
+{
+    if (head == NULL)
+    {
+        Node *val = new Node(x);
+        return val;
+    }
+    Node *val = new Node(x);
+    Node *temp = head;
+    while (temp->next != nullptr)
+    {
+        temp = temp->next;
+    }
+    temp->next = val;
+    return head;
+}
+
+int main()
+{
+    vector<int> arr = {1, 2, 3, 45, 676};
+    Node *head = ConvertArrToLL(arr);
+    int val = 5;
+    head = insertElAtEnd(head, val);
+    printLL(head);
+}

9aed1972df4398a33adea830ebf32a16943975b1
shreyamali01 <shreya.mali@iitgn.ac.in>
2025-06-08 16:44:30 +0530

    singly linked list

---
+++ 10. Linked List/2D Linked List/th1.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node *back;
+
+    Node(int data1, Node *back1, Node *next1)
+    {
+        data = data1;
+        next = next1;
+        back = back1;
+    }
+
+    Node(int data1)
+    {
+        data = data1;
+        next = nullptr;
+        back = nullptr;
+    }
+};
+
+int main()
+{
+    Node *x = new Node(3);
+    cout << x->next;
+}

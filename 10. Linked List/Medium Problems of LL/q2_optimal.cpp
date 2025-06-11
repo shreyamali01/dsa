59a9a0f976a85d35115b05200b4e82d3058cf127
shreyamali01 <shreya.mali@iitgn.ac.in>
2025-06-11 16:56:19 +0530

    reverse a 1D linked list iteratively

---
+++ 10. Linked List/Medium Problems of LL/q2_optimal.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    ListNode *reverseList(ListNode *head)
+    {
+        if (head == NULL)
+        {
+            return head;
+        }
+        if (head->next == nullptr)
+        {
+            return head;
+        }
+        ListNode *temp = head;
+        ListNode *prev = NULL;
+        while (temp != NULL)
+        {
+            ListNode *front = temp->next;
+            temp->next = prev;
+            prev = temp;
+            temp = front;
+        }
+        return prev;
+    }
+};

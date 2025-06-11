8d80924a92f59a3baa8deef3605b694e89a43210
shreyamali01 <shreya.mali@iitgn.ac.in>
2025-06-11 19:02:13 +0530

    loop in ll, TC - O(N) SC - O(1)

---
+++ 10. Linked List/Medium Problems of LL/q3_optimal.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <map>
+using namespace std;
+
+// reverse the linked list recurssively
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int data1, Node *next1)
+    {
+        data = data1;
+        next = next1;
+    }
+
+    Node(int data1)
+    {
+        data = data1;
+        next = nullptr;
+    }
+};
+
+Node *ConvertArrToLL(vector<int> &arr)
+{
+    Node *head = new Node(arr[0]);
+    Node *mover = head;
+    for (int i = 1; i < arr.size(); i++)
+    {
+        Node *temp = new Node(arr[i]);
+        mover->next = temp;
+        mover = temp;
+    }
+    return head;
+}
+
+void printLL(Node *head)
+{
+    Node *temp = head;
+    while (temp)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
+
+bool hasCycle(Node *head)
+{
+    Node *slow = head;
+    Node *fast = head;
+    while (fast != NULL && fast->next != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+int main()
+{
+    vector<int> arr = {3, 2, 0, -4};
+    Node *head = ConvertArrToLL(arr);
+    bool ans = hasCycle(head);
+    cout << ans;
+}

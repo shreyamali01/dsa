ac9b4e861e40e33604489e73f2502e4baec3cc45
shreyamali01 <shreya.mali@iitgn.ac.in>
2025-06-13 17:13:16 +0530

    intersection of nodes optimal approach TC - O(n1+n2) SC - O(1)

---
+++ 10. Linked List/Medium Problems of LL/q13_optimal.cpp
@@ -0,0 +1,106 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <map>
+using namespace std;
+
+// getIntersection Node
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int data1, Node *next1)
+    {
+        data = data1;
+        next = next1;
+    }
+
+    Node(int data1)
+    {
+        data = data1;
+        next = nullptr;
+    }
+};
+
+Node *ConvertArrToLL(vector<int> &arr)
+{
+    Node *head = new Node(arr[0]);
+    Node *mover = head;
+    for (int i = 1; i < arr.size(); i++)
+    {
+        Node *temp = new Node(arr[i]);
+        mover->next = temp;
+        mover = temp;
+    }
+    return head;
+}
+
+void printLL(Node *head)
+{
+    Node *temp = head;
+    while (temp)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
+
+Node *getIntersectionNode(Node *headA, Node *headB)
+{
+    if (headA == NULL || headB == NULL)
+    {
+        return NULL;
+    }
+
+    Node *tempA = headA;
+    Node *tempB = headB;
+    while (tempA != tempB)
+    {
+        tempA = tempA->next;
+        tempB = tempB->next;
+
+        if (tempA == tempB)
+        {
+            return tempA;
+        }
+
+        if (tempA == NULL)
+        {
+            tempA = headB;
+        }
+        if (tempB == NULL)
+        {
+            tempB = headA;
+        }
+    }
+    return tempA;
+}
+
+int main()
+{
+    // Shared part
+    Node *common = new Node(8);
+    common->next = new Node(4);
+    common->next->next = new Node(5);
+
+    // First list: 4 -> 1 -> [8 -> 4 -> 5]
+    Node *list1 = new Node(4);
+    list1->next = new Node(1);
+    list1->next->next = common;
+
+    // Second list: 5 -> 6 -> 1 -> [8 -> 4 -> 5]
+    Node *list2 = new Node(5);
+    list2->next = new Node(6);
+    list2->next->next = new Node(1);
+    list2->next->next->next = common;
+
+    Node *ans = getIntersectionNode(list1, list2);
+    if (ans)
+        cout << "Intersection at node with data: " << ans->data << endl;
+    else
+        cout << "No intersection\n";
+
+    return 0;
+}

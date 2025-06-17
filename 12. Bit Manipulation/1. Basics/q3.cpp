496fe0587c2ab72dfb9b316488c98a41d3000b2f
shreyamali01 <shreya.mali@iitgn.ac.in>
2025-06-17 21:24:23 +0530

    swap two numbers without using a third variable

---
+++ 12. Bit Manipulation/1. Basics/q3.cpp
@@ -0,0 +1,10 @@
+// pair<int, int> get(int a, int b)
+// {
+//     a = a ^ b;
+//     b = a ^ b;
+//     a = a ^ b;
+//     pair<int, int> ans;
+//     ans.first = a;
+//     ans.second = b;
+//     return ans;
+// }

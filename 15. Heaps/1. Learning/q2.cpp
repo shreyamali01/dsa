96bf797d8abeda28d6c1ab86795403e79db725d1
shreyamali01 <shreya.mali@iitgn.ac.in>
2025-07-14 15:33:52 +0530

    deleting from a heap TC - O(log n)

---
+++ 15. Heaps/1. Learning/q2.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+class heap
+{
+public:
+    int arr[100];
+    int size;
+
+    heap()
+    {
+        arr[0] = -1;
+        size = 0;
+    }
+
+    void insert(int val)
+    {
+        size = size + 1;
+        int index = size;
+        arr[index] = val;
+        while (index > 1)
+        {
+            int parent = index / 2;
+            if (arr[index] > arr[parent])
+            {
+                swap(arr[index], arr[parent]);
+                index = parent;
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+
+    // deleting the root node
+    void deletefromHeap()
+    {
+        if (size == 0)
+        {
+            cout << "nothing to delete";
+            return;
+        }
+
+        arr[1] = arr[size];
+        size--;
+
+        int i = 1;
+        while (i < size)
+        {
+            int leftIndex = 2 * i;
+            int rightIndex = 2 * i + 1;
+
+            if (leftIndex < size && arr[leftIndex] > arr[i])
+            {
+                swap(arr[leftIndex], arr[i]);
+                i = leftIndex;
+            }
+            else if (rightIndex < size && arr[rightIndex] > arr[i])
+            {
+                swap(arr[rightIndex], arr[i]);
+                i = rightIndex;
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+
+    void print()
+    {
+        for (int i = 1; i <= size; i++)
+        {
+            cout << arr[i] << " ";
+        }
+    }
+};
+
+int main()
+{
+    heap h;
+    h.insert(50);
+    h.insert(55);
+    h.insert(53);
+    h.insert(52);
+    h.insert(54);
+    h.deletefromHeap();
+    h.print();
+}
